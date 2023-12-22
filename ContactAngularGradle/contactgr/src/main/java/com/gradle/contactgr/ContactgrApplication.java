1ccb7941cd9e726d6d5383709ff7010cf1404629
merghavikavin <59721579+merghavikavin@users.noreply.github.com>
2023-12-22 18:32:11 +0530

    Add files via upload

---
+++ ContactAngularGradle/contactgr/src/main/java/com/gradle/contactgr/ContactgrApplication.java
@@ -0,0 +1,13 @@
+package com.gradle.contactgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactgrApplication.class, args);
+	}
+
+}
---
+++ ContactAngularGradle/contactgr/src/test/java/com/gradle/contactgr/ContactgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradle.contactgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
